b09c4f7e375a75c82839d99f6c6b2441e2a9d0d9
tale-fau <tale-fau@24eme.fr>
2024-12-17 14:12:42 +0100

    On a juste a saisir l'id du lien pour recuperer le json et executer les scripts

--- export.sh
+++ export.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
 if (( $# != 6 )); then
-  >&2 echo "./export.sh <fichier json> <raison sociale> <siret> <cdp> <fichier output ift> <fichier output total>"
+  >&2 echo "./export.sh <clé> <raison sociale> <siret> <cdp> <dossier_output>"
   exit 2
 fi
 
-php script_ift.php $1 $2 $3 $4 $5
-php script_total.php $1 $2 $3 $4 $6
+CLE=$1
+RAISON_SOCIALE=$2
+SIRET=$3
+CDP=$4
+DIR_OUTPUT=$5
+
+DIRNAME_TMP_IFT=/tmp/export_ift
+
+JSON_FILE=$DIRNAME_TMP_IFT/$(date +%Y%m%d%H%M%S)"_"$CDP".json"
+
+mkdir $DIRNAME_TMP_IFT 2> /dev/null
+
+curl "https://alim.api.agriculture.gouv.fr/ift/v5/api/ift/bilan/verifier/$CLE" > $JSON_FILE
+
+php script_ift.php $JSON_FILE $RAISON_SOCIALE $SIRET $CDP $DIR_OUTPUT
+php script_total.php $JSON_FILE $RAISON_SOCIALE $SIRET $CDP $DIR_OUTPUT
